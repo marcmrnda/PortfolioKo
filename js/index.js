1f4b6767aef98f0b75941ba5ff0d6162c191a353
marcmrnda <mirandamf@students.nu-lipa.edu.ph>
2024-09-20 12:00:28 +0800

    init

--- js/index.js
+++ js/index.js
@@ -217,3 +217,9 @@ $(function(){
 })
 
 
+jQuery(document).ready(function ($) {
+  $(".slider-img").on("click", function () {
+    $(".slider-img").removeClass("active");
+    $(this).addClass("active");
+  });
+});
